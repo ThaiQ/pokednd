b5dc318e61bceb6c0e3a7ea8abf16a364cbd8130
ThaiQ <quachhongthai200@gmail.com>
2018-11-16 00:37:50 -0800

    almost

--- src/App.js
+++ src/App.js
@@ -2,27 +2,200 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var spdamage, damage, mod, fdamage, fspdamage;
+var lowerd, higherd, lowersd, highersd;
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  constructor(){
+    super();
+    this.state={
+    }
   }
+
+level(event){
+  this.setState({
+    lv:event.target.value,
+  })
+}
+power(event){
+  this.setState({
+    power:event.target.value,
+  })
+}
+def(event){
+  this.setState({
+    def:event.target.value,
+  })
+}
+at(event){
+  this.setState({
+    at:event.target.value,
+  })
+}
+sat(event){
+  this.setState({
+    sat:event.target.value,
+  })
+}
+sdef(event){
+  this.setState({
+    sdef:event.target.value,
+  })
+}
+
+crit(event){
+  this.setState({
+    crit:event.target.value,
+  })
+}
+weather(event){
+  this.setState({
+    weather:event.target.value,
+  })
+}
+target(event){
+  this.setState({
+    target:event.target.value,
+  })
+}
+stab(event){
+  this.setState({
+    stab:event.target.value,
+  })
+}
+type(event){
+  this.setState({
+    type:event.target.value,
+  })
+}
+burn(event){
+  this.setState({
+    burn:event.target.value,
+  })
+}
+other(event){
+  this.setState({
+    other:event.target.value,
+  })
+}
+
+render(){
+
+  function damagec(power, def, spdef, att, spatt, lv){
+    var power = + power;
+    var def = + def;
+    var spdef = + spdef;
+    var att = + att;
+    var spatt = + spatt
+    var lv = + lv;
+    var dice = + dice;
+    damage = (((((2*lv)/5)+2)*power*(att/def))/50)+2;
+    damage = parseInt(damage);
+  }
+  function spdamagec(power, def, spdef, att, spatt, lv){
+    var power = + power;
+    var def = + def;
+    var spdef = + spdef;
+    var att = + att;
+    var spatt = + spatt
+    var lv = + lv;
+    var dice = + dice;
+    spdamage = (((((2*lv)/5)+2)*power*(spatt/spdef))/50)+2;
+    spdamage = parseInt(spdamage);
+  }
+
+  function modc(other){
+    var crit = document.getElementById("crit");
+    var target = document.getElementById("target");
+    var weather = document.getElementById("weather");
+    var burn = document.getElementById("burn");
+    var stab = document.getElementById("stab");
+    var type = document.getElementById("type");
+    var other = +other;
+
+    if (crit != null) {crit = +crit.value;}else {crit = null;}
+    if (target != null) {target = +target.value;}else {target = null;}
+    if (weather != null) {weather = +weather.value;}else {weather = null;}
+    if (burn != null) {burn = +burn.value;}else {burn = null;}
+    if (stab != null) {stab = +stab.value;}else {stab = null;}
+    if (type != null) {type = +type.value;}else {type = null;}
+
+    mod=crit*target*weather*burn*stab*type*other;
+  }
+
+  function random(min,max){return Math.floor(Math.random()*(max-min+1)+min);}
+
+  function finaldamage(power, def, spdef, att, spatt, lv, other){
+    damagec(power, def, spdef, att, spatt, lv);
+    spdamagec(power, def, spdef, att, spatt, lv);
+    modc(other);
+    lowerd=damage*mod*0.85;
+    lowersd=spdamage*mod*0.85;
+    lowerd=damage*mod*1;
+    lowersd=spdamage*mod*1;
+    damage=random(lowerd,higherd);
+    spdamage=random(lowersd,highersd);
+  }
+
+
+  return(
+    <div className="App">
+      <div>
+
+        <select id="crit" onChange={this.crit.bind(this)}>
+        <option value="0" default>crit</option>
+        <option value="1" default>no</option>
+        <option value="1.5">yes</option>
+        </select>
+        <select id="target" onChange={this.target.bind(this)}>
+        <option value="0" default>target</option>
+        <option value="1" default>1</option>
+        <option value="0.75">2 or more</option>
+        </select>
+        <select id="weather" onChange={this.weather.bind(this)}>
+        <option value="0" default>weather</option>
+        <option value="1" default>default</option>
+        <option value="1.5">advantage</option>
+        <option value="0.5">disadvantage</option>
+        </select>
+        <select id="stab" onChange={this.stab.bind(this)}>
+        <option value="0" default>stab</option>
+        <option value="1" default>default</option>
+        <option value="1.5" default>match</option>
+        <option value="2">adaptability</option>
+        </select>
+        <select id="stab" onChange={this.type.bind(this)}>
+        <option value="0" default>type</option>
+        <option value="1" default>normal</option>
+        <option value="2" default>super effective</option>
+        <option value="4" default>super super effective</option>
+        <option value="0.25">not very effective</option>
+        <option value="0">ineffective</option>
+        </select>
+        <select id="burn" onChange={this.burn.bind(this)}>
+        <option value="0" default>burn</option>
+        <option value="1" default>no</option>
+        <option value="0.5">yes</option>
+        </select>
+
+
+      Name: <input type="text"/>
+      lv<input type="number" onChange={this.level.bind(this)}/>
+      pwr<input type="number" onChange={this.power.bind(this)}/>
+      def<input type="number" onChange={this.def.bind(this)}/>
+      sp.def<input type="number" onChange={this.sdef.bind(this)}/>
+      att<input type="number" onChange={this.at.bind(this)}/>
+      sp.att<input type="number" onChange={this.sat.bind(this)}/>
+      other<input type="number" onChange={this.other.bind(this)}/>
+      </div>
+
+      <h1>{finaldamage(this.state.power,this.state.def,this.state.sdef,this.state.at,this.state.sat,this.state.lv,this.state.other)}Damage: {damage}</h1>
+      <h1>Special Damage: {spdamage}{damage}{lowerd}{higherd}</h1>
+      <button onClick={finaldamage(this.state.power,this.state.def,this.state.sdef,this.state.at,this.state.sat,this.state.lv,this.state.other)}>c</button>
+    </div>
+  );
+}
 }
 
 export default App;
